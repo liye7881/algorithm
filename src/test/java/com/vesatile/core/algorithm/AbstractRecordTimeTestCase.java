0bb124389c8d48892f985f9373d6976204689090
ye <liye7881@gmail.com>
2013-05-05 18:21:35 +0800

    ListTest

--- src/test/java/com/vesatile/core/algorithm/AbstractRecordTimeTestCase.java
+++ src/test/java/com/vesatile/core/algorithm/AbstractRecordTimeTestCase.java
@@ -6,8 +6,39 @@ import java.text.NumberFormat;
 import org.apache.log4j.Logger;
 
 public abstract class AbstractRecordTimeTestCase {
+	public interface InvokeInternal {
+		public Object execute();
+	}
+
 	private static Logger logger = Logger
 			.getLogger(AbstractRecordTimeTestCase.class);
+
+	private long currentTime(boolean nano) {
+		long result = System.currentTimeMillis();
+		if (nano) {
+			result = System.nanoTime();
+		}
+		return result;
+	}
+
+	protected Object execute(InvokeInternal invokeInternal) {
+		return execute(invokeInternal, true);
+	}
+
+	protected Object execute(InvokeInternal invokeInternal, boolean record) {
+		long start = currentTime(true);
+
+		Object result = invokeInternal.execute();
+
+		if (record && logger.isInfoEnabled()) {
+			String cost = NumberFormat.getInstance().format(
+					currentTime(true) - start);
+			String message = "Execute in " + cost;
+			logger.info(message);
+		}
+
+		return result;
+	}
 
 	protected Object execute(Object target, String methodName, Object[] args) {
 		Object result = null;
@@ -51,14 +82,6 @@ public abstract class AbstractRecordTimeTestCase {
 					+ (currentTime(nano) - start));
 		}
 
-		return result;
-	}
-
-	private long currentTime(boolean nano) {
-		long result = System.currentTimeMillis();
-		if (nano) {
-			result = System.nanoTime();
-		}
 		return result;
 	}
 }
--- src/test/java/com/vesatile/core/algorithm/ListTest.java
+++ src/test/java/com/vesatile/core/algorithm/ListTest.java
@@ -1,22 +1,161 @@
 package com.vesatile.core.algorithm;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class ListTest extends AbstractRecordTimeTestCase {
+	private class Make0 implements InvokeInternal {
+		private List<Long> list;
+		private Long count;
 
-	protected void make(List<Integer> list, int n) {
-		for (int i = 0; i < n; i++) {
+		public Make0(List<Long> list, Long count) {
+			this.list = list;
+			this.count = count;
+		}
+
+		@Override
+		public Object execute() {
+			make0(list, count);
+			return null;
+		}
+	}
+
+	private class Make1 implements InvokeInternal {
+		private List<Long> list;
+		private Long count;
+
+		public Make1(List<Long> list, Long count) {
+			this.list = list;
+			this.count = count;
+		}
+
+		@Override
+		public Object execute() {
+			make1(list, count);
+			return null;
+		}
+	}
+
+	private class Remove0 implements InvokeInternal {
+		private List<Long> list;
+
+		public Remove0(List<Long> list) {
+			this.list = list;
+		}
+
+		@Override
+		public Object execute() {
+			remove0(list);
+			return null;
+		}
+	}
+
+	private class Remove1 implements InvokeInternal {
+		private List<Long> list;
+
+		public Remove1(List<Long> list) {
+			this.list = list;
+		}
+
+		@Override
+		public Object execute() {
+			remove1(list);
+			return null;
+		}
+	}
+
+	private class Sum0 implements InvokeInternal {
+		private List<Long> list;
+
+		public Sum0(List<Long> list) {
+			this.list = list;
+		}
+
+		@Override
+		public Object execute() {
+			return sum(list);
+		}
+	}
+
+	private static Logger logger = Logger.getLogger(ListTest.class);
+
+	public void make0(List<Long> list, Long n) {
+		list.clear();
+		for (long i = 0; i < n; i++) {
 			list.add(i);
 		}
 	}
 
+	public void make1(List<Long> list, Long n) {
+		list.clear();
+		for (long i = 0; i < n; i++) {
+			list.add(0, i);
+		}
+	}
+
+	public void remove0(List<Long> list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) % 2 == 0) {
+				list.remove(i);
+			}
+		}
+	}
+
+	public void remove1(List<Long> list) {
+		Iterator<Long> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			Long value = (Long) iterator.next();
+
+			if (value % 2 == 0) {
+				iterator.remove();
+			}
+		}
+	}
+
+	public long sum(List<Long> list) {
+		long sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		return sum;
+	}
+
 	@Test
 	public void test() {
-		execute(this, "make", new Object[] { new ArrayList<Integer>(), 10000 });
-		execute(this, "make", new Object[] { new LinkedList<Integer>(), 10000 });
+		final long count = 200000L;
+
+		ArrayList<Long> arrayList = new ArrayList<Long>();
+		LinkedList<Long> linkedList = new LinkedList<Long>();
+
+		info("Make 0");
+		execute(new Make0(arrayList, count));
+		execute(new Make0(linkedList, count));
+
+		info("Remove 0");
+		execute(new Remove0(arrayList));
+		execute(new Remove0(linkedList));
+
+		info("Make 1");
+		execute(new Make1(arrayList, count));
+		execute(new Make1(linkedList, count));
+
+		info("Sum 0");
+		execute(new Sum0(arrayList));
+		execute(new Sum0(linkedList));
+
+		info("Remove 1");
+		execute(new Remove1(arrayList));
+		execute(new Remove1(linkedList));
+	}
+
+	private void info(String message) {
+		if (logger.isInfoEnabled()) {
+			logger.info(message);
+		}
 	}
 }
